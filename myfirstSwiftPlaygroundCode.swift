58118b9b07ec414c5deb7a72eda213b5227ae88c
mail2sanket <127780380+mail2sanket@users.noreply.github.com>
2023-03-13 23:10:20 +0530

    Creating my first ever git commit
    This file contains my first swift learning... Hola!!!
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

---
+++ myfirstSwiftPlaygroundCode.swift
@@ -0,0 +1,48 @@
+//Paste this in XCode playground
+
+// wao program to to print a number
+
+print(10)
+
+// wap to store value 20 in a variable and print the value of variable
+
+var akka = 20
+
+print(akka)
+
+// create one more variable and store 50
+
+var amma = 50
+
+print(amma)
+
+// noow create one more variable and store sum of amma and akka
+
+var papa = amma + akka
+
+print(papa)
+
+
+
+// adding amma, akka and papa and storing in sanku
+
+var sanku = amma + akka + papa
+
+print(sanku)
+
+// letting a program decided statment true or false
+    
+let mangoesAreSweet = false
+let ApplesMakeYouUnhealthy = true
+
+if mangoesAreSweet {
+   print("Mangoes are Indias National fruit")
+} else {
+    print("Hey berries looked like mangoes, they were not sweet!!!")
+}
+
+if ApplesMakeYouUnhealthy {
+    print("My God!, Whole life I was taught incorrect things")
+}else {
+    print("What was taught to me when I was young is correct", "An apple a day kees doctor away!!!")
+}
